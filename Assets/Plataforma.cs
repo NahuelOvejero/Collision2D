525a6da635ba6df976a065fbb502f5c24f019901
Nahuel <nahuelovejero_70@hotmail.com>
2017-07-17 17:34:49 -0300

    Collisions 2D

---
+++ Assets/Plataforma.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plataforma : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D colision)
+    {
+        print("Colision!");
+    }
+    void OnTriggerEnter2D(Collider2D colider) {
+        print("Triggered!");
+    }
+
+}
